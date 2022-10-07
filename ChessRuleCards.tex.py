9cddc22eb65353d65ba2bf8ed766af2e95008239
Thomas Draper <tgdrape@gmail.com>
2022-10-07 14:06:34 -0700

    Update some rule cards. Change output format to standard card size (MTG) instead of Dominion card size.

--- ChessRuleCards.tex.py
+++ ChessRuleCards.tex.py
@@ -123,6 +123,83 @@ def buildTeX(cards):
             print(r'\\[-\lineskip]')
     print(TEX_FOOTER)
 
+def Magic_TikZ_card(card):
+    CARD_TEMPLATE = [
+    r'''\begin{tikzpicture}
+    \pgfmathsetmacro{\cardroundingradius}{5mm}
+    \pgfmathsetmacro{\striproundingradius}{3mm}
+    % \pgfmathsetmacro{\cardwidth}{5.9}
+    % \pgfmathsetmacro{\cardheight}{9.2}
+    \pgfmathsetmacro{\cardwidth}{6.1}  % Magic cards are 63x88mm
+    \pgfmathsetmacro{\cardheight}{8.6}
+    \pgfmathsetmacro{\stripwidth}{1.2}
+    \pgfmathsetmacro{\strippadding}{0.1}
+    \pgfmathsetmacro{\textpadding}{0.3}
+    \pgfmathsetmacro{\ruleheight}{0.1}
+    \providecommand{\stripfontsize}{\Huge}
+    \providecommand{\captionfontsize}{\LARGE}
+    \providecommand{\textfontsize}{\Large}
+    \providecommand{\quotefontsize}{\small}
+    \draw[line width=2mm,rounded corners=\cardroundingradius] (0,0) rectangle (\cardwidth,\cardheight);
+    \draw[line width=2mm] (0,0) rectangle (\cardwidth,\cardheight);
+    \node[text width=(\cardwidth-\strippadding-2*\textpadding)*1cm,below right,inner sep=0] at (\strippadding+\textpadding,\cardheight-\textpadding) 
+    { 
+    \begin{center} {\fontsize{80pt}{60pt}\selectfont ''',
+    r'''}\\\end{center}
+\begin{center}
+    {\captionfontsize \textsf{\textbf{''',
+    r'''}}}\end{center}
+        {\textfontsize ''',
+    r'''}\\
+        \tikz{\fill (0,0) rectangle (\cardwidth-2*\strippadding-2*\textpadding,\ruleheight);}\\
+        {\quotefontsize \textit{''',
+    r'''}}\\[-2\baselineskip]
+    };
+    \node[circle,draw,text=black](c) at (.5,\cardheight-.5){''',
+    r'''};
+    \end{tikzpicture}%''',
+    ]
+
+    origin = '?'
+    if 'origin' in card:
+        origin = card['origin']
+    CARD_DATA = [card['symbol'],card['caption'], card['description'], card['quote'], origin]
+    # CARD_DATA = [card['stripcolor'], striptext, card['caption'], card['description'], card['bottomcaption'], card['quote']]
+    CARD_TEX = itertools.chain(*itertools.zip_longest(CARD_TEMPLATE, CARD_DATA, fillvalue=''))
+    # for v in CARD_TEX:
+        # print(v)
+    # print((CARD_TEX))
+    print(''.join(CARD_TEX))
+
+def buildMagicTeX(cards):
+    TEX_HEADER = r'''
+    % latexmk -pdflatex='lualatex' -pdf FontExhibition.tex
+    \documentclass[parskip,landscape,letter]{scrartcl}
+    \usepackage{fontspec}
+    \usepackage[margin=10mm,left=20mm]{geometry}
+    \usepackage{tikz}
+    \usetikzlibrary{matrix,backgrounds}
+    \usepackage{pifont}
+    \usepackage{graphicx}
+    \usepackage{chessfss}
+    \usepackage{setspace}
+
+    \begin{document}
+    \setmainfont[Extension={.ttf},ItalicFont={DejaVuSerif-Italic}]{FreeSerif}
+    '''
+
+    TEX_FOOTER = r'''
+    \end{document}
+    '''
+    print(TEX_HEADER)
+    for i, card in enumerate(cards):
+        # TikZ_sidebar_card(card)
+        Magic_TikZ_card(card)
+        print(r'\hspace{1pt}%')
+        if i%4==3:
+            print(r'\\[-.5\lineskip]')
+    print(TEX_FOOTER)
+
 if __name__ == '__main__':
     import json
 
@@ -133,5 +210,6 @@ if __name__ == '__main__':
         json_dict = json.load(infile)
     cards = json_dict['text_cards']
     gcards = json_dict['graphic_cards']
-    buildTeX(cards)
+    buildMagicTeX(cards)
+    # buildTeX(cards)
     # buildTeX(gcards)
